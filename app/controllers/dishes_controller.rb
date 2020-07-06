4c3ef69eed375c6101b1074bdd43e5cba90151d4
Yun Chung <yun.chung@mac.com>
2020-07-06 16:56:08 +0900

    Created a new dish view page and added code to the dish controller to create a new dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,23 @@ class DishesController < ApplicationController
   def index
     @dishes = Dish.all
   end
+
+  def new
+    @dish = Dish.new
+  end
+
+  def create
+    @dish = Dish.new(dish_params)
+    if @dish.save
+      redirect_to dish_path(@dish)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def dish_params
+    params.require(:dish).permit(:name, :photo, :instructions, :description)
+  end
 end
