f5282d2ed356f147029913b621764e875a61d99c
Yun Chung <yun.chung@mac.com>
2020-10-23 09:19:52 +0900

    Added MongoDB db directory to server directory

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const db = require('./db')
+
 const app = express()
 const apiPort = 3000
 
@@ -8,8 +11,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 app.get('/', (req, res) => {
-  res.send('Welcome to Instant Pot Recipes!')
+  res.send('Instant Pot Recipes connected...')
 })
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
