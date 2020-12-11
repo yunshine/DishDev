8eca0a30162e0420c062c2b6793174e249f8ad13
Yun Chung <yun.chung@mac.com>
2020-12-11 15:30:21 +0900

    Reformat of recipe and user model files

--- server/models/recipe-model.js
+++ server/models/recipe-model.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// should be recipeSchema...
-const Recipe = new Schema(
-    {
-        name: { type: String, required: true },
-        ingredients: { type: [String], required: true },
-        // rating: { type: Number, required: true },
+const recipeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    images: { type: [String], required: true },
+    ingredients: { type: [String], required: true },
+    directions: { type: [String], required: true },
+    // rating: { type: Number, required: true },
 
-        // author: {
-        //   id: {
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     ref: "User"
-        //   },
-        //   username: String
-        // },
+    // author: {
+    //   id: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "User"
+    //   },
+    //   username: String
+    // },
 
-    },
+},
     { timestamps: true },
-)
+);
 
 // 'recipes' should be uppercase and singular, and Recipe should be recipeSchema...
-module.exports = mongoose.model('recipes', Recipe)
+module.exports = mongoose.model('Recipe', recipeSchema);
