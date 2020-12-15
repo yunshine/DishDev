2bbd06f0239c7afac5c7f4d94d5982cca6e359d1
Yun Chung <yun.chung@mac.com>
2020-12-15 16:29:59 +0900

    Updated index.js file in the pages directory to export components

---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+import RecipesList from './RecipesList';
+import RecipesInsert from './RecipesInsert';
+import RecipesUpdate from './RecipesUpdate';
+
+export { RecipesList, RecipesInsert, RecipesUpdate };
