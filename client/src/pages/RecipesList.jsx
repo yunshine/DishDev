703ba4c32128c416ce8beed6d4e1929b3dd88f14
Yun Chung <yun.chung@mac.com>
2020-12-15 16:20:58 +0900

    Basic RecipesList component template created

---
+++ client/src/pages/RecipesList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RecipesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>In this page you'll see the list of recipes</p>
+            </div>
+        );
+    };
+};
+
+export default RecipesList;
