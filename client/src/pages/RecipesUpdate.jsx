37b91f3831d4fae404c54497fa4637bfe86ef0f0
Yun Chung <yun.chung@mac.com>
2020-12-15 16:22:35 +0900

    Basic RecipesUpdate component template created

---
+++ client/src/pages/RecipesUpdate.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RecipesUpdate extends Component {
+    render() {
+        return (
+            <div>
+                <p>In this page you'll see the form to update a recipes</p>
+            </div>
+        );
+    };
+};
+
+export default RecipesUpdate;
