ce94bdcfa70d099e2bc64dc04d82693861cf4ce0
Yun Chung <yun.chung@mac.com>
2020-12-17 14:10:54 +0900

    Able to display all recipe data in the RecipeList component

--- client/src/pages/RecipesList.jsx
+++ client/src/pages/RecipesList.jsx
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
-// import ReactTable from 'react-table';
 import api from '../api';
 
 import styled from 'styled-components';
-
-// import 'react-table/react-table.css';
-
-// const Wrapper = styled.div`
-//     padding: 0 40px 40px 40px;
-// `
 
 class RecipesList extends Component {
     constructor(props) {
@@ -36,61 +29,25 @@ class RecipesList extends Component {
         console.log('TCL: RecipesList -> render -> recipes', recipes)
         console.log("Loading? ", this.state.isLoading)
 
-        // const columns = [
-        //     {
-        //         Header: 'ID',
-        //         accessor: '_id',
-        //         filterable: true,
-        //     },
-        //     {
-        //         Header: 'Name',
-        //         accessor: 'name',
-        //         filterable: true,
-        //     },
-        //     {
-        //         Header: 'Rating',
-        //         accessor: 'rating',
-        //         filterable: true,
-        //     },
-        //     {
-        //         Header: 'Time',
-        //         accessor: 'time',
-        //         Cell: props => <span>{props.value.join(' / ')}</span>,
-        //     },
-        // ];
-
-        // let showTable = true;
-        // if (!recipes.length) {
-        //     showTable = false;
-        // };
-
-
-
-
         let testText = <p>testText...</p>;
         if (recipes.length) {
-            testText = <p>There are some recipes here...</p>;
+            testText = <p>Great! Some recipes exist...</p>;
         } else {
-            testText = <p>There are no recipes</p>;
+            testText = <h4>!!!Oh NO!!! ERROR!!! There are no recipes!!!</h4>;
         };
 
         return (
-            // <Wrapper>
-            //     {showTable && (
-            //         <ReactTable
-            //             data={recipes}
-            //             columns={columns}
-            //             loading={isLoading}
-            //             defaultPageSize={10}
-            //             showPageSizeOptions={true}
-            //             minRows={0}
-            //         />
-            //     )}
-            // </Wrapper>
-
             <div>
                 <p>In this page you'll see the list of recipes</p>
                 {testText}
+                {recipes.map(recipe => (
+                    <>
+                        <p>{recipe.name}</p>
+                        <p>{recipe.images}</p>
+                        <p>{recipe.ingredients}</p>
+                        <p>{recipe.directions}</p>
+                    </>
+                ))}
             </div>
         );
     };
