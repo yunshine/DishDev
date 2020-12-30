68fef96a8dfabb57b13e19a1a2533ffc4d258f3f
Yun Chung <yun.chung@mac.com>
2020-12-30 12:02:45 +0900

    Fixed links in navbar

--- client/src/components/Links.jsx
+++ client/src/components/Links.jsx
@@ -1,56 +1,57 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Collapse = styled.div.attrs({
-    className: 'collpase navbar-collapse',
-})``
+// const Collapse = styled.div.attrs({
+//     className: 'collpase navbar-collapse',
+// })``
 
-const List = styled.div.attrs({
-    className: 'navbar-nav mr-auto',
-})``
+// const List = styled.div.attrs({
+//     className: 'navbar-nav mr-auto',
+// })``
 
-const Item = styled.div.attrs({
-    className: 'collpase navbar-collapse',
-})``
+// const Item = styled.div.attrs({
+//     className: 'collpase navbar-collapse',
+// })``
 
 class Links extends Component {
     render() {
         return (
             <div>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <Link to="/" class="navbar-brand">Home</Link>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            <li class="nav-item">
+                                <Link to="/recipes/list" class="nav-link">See All Recipes</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                                <Link to="/recipes/create" class="nav-link">Create a New Recipe</Link>
                             </li>
-                            <li class="nav-item dropdown">
+
+                            {/* <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Dropdown
-        </a>
+                                </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="#">Action</a>
                                     <a class="dropdown-item" href="#">Another action</a>
                                     <div class="dropdown-divider"></div>
                                     <a class="dropdown-item" href="#">Something else here</a>
                                 </div>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                            </li>
+                            </li> */}
+
                         </ul>
-                        <form class="form-inline my-2 my-lg-0">
+
+                        {/* <form class="form-inline my-2 my-lg-0">
                             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
+                        </form> */}
+
                     </div>
                 </nav>
 
