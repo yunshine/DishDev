0b010b7eea5fcd1f52e1d0d436a8d359a76d91bb
Yun Chung <yun.chung@mac.com>
2020-12-31 12:41:11 +0900

    Added unstyled jumbotron to root page

--- client/src/pages/Root.jsx
+++ client/src/pages/Root.jsx
@@ -12,8 +12,13 @@ class Root extends Component {
     render() {
         const { classes } = this.props;
         return (
-            <div className={classes.Root}>
-                <h3>this is the root component...</h3>
+            <div class="banner">
+                {/* <div class="banner" style="background-image: linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url(https://res.cloudinary.com/deyqjsowe/image/upload/v1609385709/Instant%20Pot%20Recipes/InstantPotBanner_gz2qqs.jpg);"> */}
+                <div class="container">
+                    <h1>Le Wagon brings <strong>tech skills</strong> to <strong>creative people</strong>!</h1>
+                    <p>Change your life and learn to code at one of our campuses around the world.</p>
+                    {/* <a class="btn btn-flat" href="#">Apply now</a> */}
+                </div>
             </div>
         );
     };
--- client/src/styles/RootStyles.js
+++ client/src/styles/RootStyles.js
@@ -5,3 +5,5 @@ export default {
         backgroundColor: 'rgba(255,64,80, 1)',
     },
 }
+
+// style="backgroundImage: linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url(https://res.cloudinary.com/deyqjsowe/image/upload/v1609385709/Instant%20Pot%20Recipes/InstantPotBanner_gz2qqs.jpg);"
