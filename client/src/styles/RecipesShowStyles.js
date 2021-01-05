31cb4d859d898d5fdadec231c9cda96ecc324dde
Yun Chung <yun.chung@mac.com>
2021-01-05 16:37:16 +0900

    Created a responsive unstyled container for the recipe show page

--- client/src/styles/RecipesShowStyles.js
+++ client/src/styles/RecipesShowStyles.js
@@ -1,7 +1,34 @@
-// import sizes from './MediaSizes';
+import sizes from './MediaSizes';
 
 export default {
     RecipesShow: {
+        border: '5px solid red',
+        width: '1440px',
+        margin: 'auto',
+        // display: 'flex',
+        // flexWrap: 'wrap',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // position: 'relative',
+        minHeight: 'calc(100vh - 96px)',
+        // marginTop: '16px',
+        // height: 'calc(100% - 48px)',
+        // paddingBottom: '80px',
 
+        [sizes.down('xl')]: {
+            width: '1100px',
+        },
+        [sizes.down('lg')]: {
+            width: '960px',
+        },
+        [sizes.down('md')]: {
+            width: '720px',
+        },
+        [sizes.down('sm')]: {
+            width: '540px',
+        },
+        [sizes.down('xs')]: {
+            width: '100%',
+        },
     },
 }
