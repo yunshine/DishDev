c0316e092ffa1b884ed8f0d7aa350c61491064b4
Yun Chung <yun.chung@mac.com>
2021-01-12 14:20:13 +0900

    Attempt to push to Heroku

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const developmentDB = 'mongodb://127.0.0.1:27017/instant_pot_recipes';
+const dbUrl = process.env.DB_URL;
 
-mongoose.connect('mongodb://127.0.0.1:27017/instant_pot_recipes', {
+// mongoose.connect(developmentDB, {
+// mongoose.connect(dbUrl, {
+mongoose.connect('mongodb+srv://yunshine:ilJC8239@cluster0.zow7p.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    // mongoose.connect('mongodb://127.0.0.1:27017/instant_pot_recipes', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
