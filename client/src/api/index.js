359942c62e95746a350c1ba082e1448676ef9469
Yun Chung <yun.chung@mac.com>
2021-01-12 15:10:53 +0900

    Package.json update to React scripts to push to heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: process.env.PORT || 'http://localhost:3000/api',
 });
 
 export const getAllRecipes = () => api.get(`/recipes/list`);
