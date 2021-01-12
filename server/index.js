8d652aa8d68e754ed464eb147afc5c3b1bf248b9
Yun Chung <yun.chung@mac.com>
2021-01-12 14:35:37 +0900

    3rd attempt to push to heroku

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const db = require('./db');
 const recipesRouter = require('./routes/recipes-router');
 
 const app = express();
-const apiPort = 3000;
+const apiPort = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
