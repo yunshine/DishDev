ececb501533d335f5de86ce8d51eb46300f771e3
Yun Chung <yun.chung@mac.com>
2021-06-27 20:01:42 +0900

    User model edits and passport.js file

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -12,12 +12,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    role: {
-        type: String,
-        enum: ['user', 'admin'],
-        required: true
-    },
-    todos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Todo' }]
+    todos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }]
 });
 
 UserSchema.pre('save', function (next) {
