2d7f5c4eeedba1fc683e00258b3447920b07bd4e
Yun Chung <yun.chung@mac.com>
2021-07-06 12:14:17 +0900

    Added higher order component

---
+++ client/src/higherOrderComponents/PrivateRoute.js
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { Route, Redirect } from "react-router";
+import { AuthContext } from "../context/AuthContext";
+
+// This component protects the routes that you should be logged in for...
+
+// destructured props parameter and renamed...
+// const PrivateRoute = (props) => {
+const PrivateRoute = ({ component: Component, roles, ...rest }) => {
+    const { isAuthenticated, user } = useContext(AuthContext);
+
+    return (
+        <Route {...rest} render={props => {
+            if (!isAuthenticated) {
+                return <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+            }
+
+            if (!roles.includes(user.role)) {
+                return <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+            }
+
+            return <Component {...props} />
+        }} />
+    )
+}
+
+export default PrivateRoute;
