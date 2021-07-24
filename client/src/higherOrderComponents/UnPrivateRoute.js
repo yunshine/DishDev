a7d84e8e2183e74134a5df2d3729d6f16ea86cbc
Yun Chung <yun.chung@mac.com>
2021-07-24 11:17:49 +0900

    uncommented auth files

--- client/src/higherOrderComponents/UnPrivateRoute.js
+++ client/src/higherOrderComponents/UnPrivateRoute.js
@@ -1,23 +1,23 @@
-import { useContext } from "react";
-import { Route, Redirect } from "react-router";
-import { AuthContext } from "../context/AuthContext";
+// import { useContext } from "react";
+// import { Route, Redirect } from "react-router";
+// import { AuthContext } from "../context/AuthContext";
 
-// This component checks to see if you're logged in so that you don't have to ever see the login page or the register page because it wouldn't make sense to see those pages...
+// // This component checks to see if you're logged in so that you don't have to ever see the login page or the register page because it wouldn't make sense to see those pages...
 
-// destructured props parameter and renamed...
-// const UnPrivateRoute = (props) => {
-const UnPrivateRoute = ({ component: Component, ...rest }) => {
-    const { isAuthenticated } = useContext(AuthContext);
+// // destructured props parameter and renamed...
+// // const UnPrivateRoute = (props) => {
+// const UnPrivateRoute = ({ component: Component, ...rest }) => {
+//     const { isAuthenticated } = useContext(AuthContext);
 
-    return (
-        <Route {...rest} render={props => {
-            if (isAuthenticated) {
-                return <Redirect to={{ pathname: "/home", state: { from: props.location } }} />
-            }
+//     return (
+//         <Route {...rest} render={props => {
+//             if (isAuthenticated) {
+//                 return <Redirect to={{ pathname: "/home", state: { from: props.location } }} />
+//             }
 
-            return <Component {...props} />
-        }} />
-    )
-}
+//             return <Component {...props} />
+//         }} />
+//     )
+// }
 
-export default UnPrivateRoute;
+// export default UnPrivateRoute;
