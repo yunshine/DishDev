05f359b13846ad090dac9b47077c416fbd4e9588
Yun Chung <yun.chung@mac.com>
2021-07-31 19:40:21 +0900

    Created Message component

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,6 +3,7 @@ import RecipesList from './RecipesList';
 import RecipesCreate from './RecipesCreate';
 import RecipesUpdate from './RecipesUpdate';
 import RecipeShow from './RecipeShow';
+import Message from './Message';
 import Registration from './Registration';
 
-export { Root, RecipesList, RecipesCreate, RecipesUpdate, RecipeShow, Registration };
+export { Root, RecipesList, RecipesCreate, RecipesUpdate, RecipeShow, Registration, Message };
